3c9adb399e7a91a766a7693e0bb89d6b671b74e6
Damien Alexandre <mobman02@gmail.com>
2013-05-03 20:56:41 +0200

    Add some todo stuffs

--- src/JoliTypo/Fixer.php
+++ src/JoliTypo/Fixer.php
@@ -3,12 +3,15 @@ namespace JoliTypo;
 
 class Fixer
 {
-
   const NO_BREAK_THIN_SPACE = "&#8239;";
   const NO_BREAK_SPACE = "&#160;";
 
+  protected $tags_to_backup = array('pre', 'code', 'script', 'style');
+
   public function fix($content)
   {
+    $content = $this->backup($content);
+
     foreach (array('Ellipsis') as $fixer_name) {
       $class = 'JoliTypo\\Fixer\\'.$fixer_name;
       $fixer = new $class();
@@ -16,6 +19,18 @@ class Fixer
       $content = $fixer->fix($content);
     }
 
+    $content = $this->unbackup($content);
+
     return $content;
+  }
+
+  private function backup($content)
+  {
+    // @todo
+  }
+
+  private function unbackup($content)
+  {
+    // @todo
   }
 }
