778329433942b39143711be107c3bb5106492055
Damien Alexandre <mobman02@gmail.com>
2013-06-23 20:17:05 +0200

    Smalls fixes on the HTML5 test (ref #1).

--- tests/JoliTypo/Tests/Html5Test.php
+++ tests/JoliTypo/Tests/Html5Test.php
@@ -10,25 +10,11 @@ class Html5Test extends \PHPUnit_Framework_TestCase
         $fixer = new Fixer('fr_FR');
         $this->assertInstanceOf('JoliTypo\Fixer', $fixer);
 
+        $html5 = <<<HTML
+<header>header</header><aside>Dummy content.</aside><nav><ul><li>Content</li></ul></nav><article><video></video><audio></audio><canvas></canvas><figure>Content</figure></article><footer>footer</footer>
+HTML;
+
         // The test passes if there is no warning about this fix:
-        $fixer->fix("<header>header</header>
-        <aside>Dummy content.</aside>
-
-        <nav>
-            <ul><li>Content</li></ul>
-        </nav>
-        <article>
-
-            <p>Some text.</p>
-
-            <video></video>
-            <audio></audio>
-
-            <canvas></canvas>
-
-            <figure>Content.</figure>
-        </article>
-        <footer>footer</footer>
-        ");
+        $this->assertEquals($html5, $fixer->fix($html5));
     }
 }
